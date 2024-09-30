f43e7a9f6b17b812eb25b38bc5def96038a90441
Nazmi <nazmiyazkan@gmail.com>
2024-09-30 23:21:26 +0300

    logo and favicon added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,54 @@
-import { Card } from "primereact/card";
+import React from "react";
 import { InputText } from "primereact/inputtext";
 import { Password } from "primereact/password";
 import { Button } from "primereact/button";
-import { Link } from "react-router-dom";
+import { Divider } from "primereact/divider";
+import "./Login.css"; // Stil dosyası için
 
 const Login = () => {
   return (
-    <div className="flex justify-content-center align-items-center h-screen">
-      <Card title="Login" className="p-fluid" style={{ width: "400px" }}>
-        <div className="field">
-          <label htmlFor="email">Email</label>
-          <InputText id="email" type="email" placeholder="Email" />
+    <div className="login-page">
+      <div style={{ width: "800px" }} className="login-card">
+        <div className="logo">
+          <img
+            style={{ width: "200px", height: "120px" }}
+            src="/logo.png"
+            alt="Logo"
+          />
         </div>
-        <div className="field">
-          <label htmlFor="password">Password</label>
-          <Password id="password" toggleMask feedback={false} />
+        <div className="login-content">
+          <div className="login-form">
+            <h2>Login</h2>
+            <div className="input-group">
+              <label htmlFor="username">Username</label>
+              <InputText id="username" placeholder="Enter username" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <Password
+                id="password"
+                feedback={false}
+                placeholder="Enter password"
+              />
+            </div>
+
+            <Button
+              label="Login"
+              className="p-button-rounded p-button-primary"
+            />
+          </div>
+
+          <Divider layout="vertical" className="divider" />
+
+          <div className="register-section">
+            <h2>Not a member?</h2>
+            <Button
+              label="Register"
+              className="p-button-rounded p-button-secondary"
+            />
+          </div>
         </div>
-        <div className="field">
-          <Button label="Login" icon="pi pi-sign-in" className="w-full" />
-        </div>
-        <p>
-          Dont have an account? <Link to="/register">Sign up</Link>
-        </p>
-      </Card>
+      </div>
     </div>
   );
 };
